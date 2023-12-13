6c912bb53f776541354484d27b878c3f73b18694
周炳艳 <434387059@qq.com>
2023-12-13 21:53:19 +0800

    第一次提交super主题

---
+++ src/common/getModulesScopeGenerater.js
@@ -0,0 +1,18 @@
+import stringHash from "string-hash"
+
+export const getModulesScopeGenerater = (opt) => {
+  return (name, filename, css) => {
+    if ((opt.multipleScopeVars || []).some((item) => item.scopeName === name)) {
+      return name
+    }
+
+    if (typeof opt.generateScopedName === "function") {
+      return opt.generateScopedName(name, filename, css)
+    }
+
+    const i = css.indexOf(`.${name}`)
+    const lineNumber = css.substr(0, i).split(/[\r\n]/).length
+    const hash = stringHash(css).toString(36).substr(0, 5)
+    return `_${name}_${hash}_${lineNumber}`
+  }
+}
---
+++ src/toBrowerEnvs.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 周炳艳 434387059@qq.com
+ * @Date: 2023-12-13 21:12:13
+ * @LastEditors: 周炳艳 434387059@qq.com
+ * @LastEditTime: 2023-12-13 21:12:19
+ * @FilePath: /super-admin-theme/src/toBrowerEnvs.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/** 此文件的内容是临时的，会在index.js 运行时更换*/
+export const basePath = "";
+export const assetsDir = "";
+export const buildCommand = "";
+export const browerPreprocessorOptions = [];
